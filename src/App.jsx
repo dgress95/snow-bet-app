fd2824ce2e32e4f0ac6fa8eb3b3dc8c1ea736093
Diane Gress <diane.gress@criticalmass.com>
2025-02-11 17:41:41 -0600

    Deploying with Vite

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 import "./snow.css";
 
-const API_BASE_URL = "http://localhost:5001";
+const API_BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:5001";
 
 function App() {
   const [name, setName] = useState("");
